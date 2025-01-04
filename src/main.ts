eb45a4670a21551485fd776dc64c9840b4bf586c
Nick2603 <mykytavostrikov@gmail.com>
2025-01-04 12:00:43 +0200

    added compression

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { AppConfigService } from './config/app-config.service';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.use(helmet());
 
   app.enableCors();
+
+  app.use(compression());
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
